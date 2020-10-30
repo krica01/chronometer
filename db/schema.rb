a0c894b57fab7305a989a4f013965a9fee3218ae
Jan Kricka <jankricka@Jans-MacBook-Pro-2.local>
2020-10-30 23:34:28 +0100

    Updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_24_210552) do
+ActiveRecord::Schema.define(version: 2020_05_28_202828) do
+
+  create_table "race_permissions", force: :cascade do |t|
+    t.integer "race_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "racers", force: :cascade do |t|
     t.integer "race_id"
